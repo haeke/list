a4061bdde1fb0dd30a22de494b92f6d6539b5812
haeke <riverae2@student.wpunj.edu>
2019-04-15 18:39:46 -0400

    Updates

---
+++ precache-manifest.702c31de5ad032ce3f6ef8845d40bf33.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "893017e7fd6b6b846040",
+    "url": "/list/static/js/runtime~main.7fc17663.js"
+  },
+  {
+    "revision": "83404bcc06fe7c695f1c",
+    "url": "/list/static/js/main.5405cc62.chunk.js"
+  },
+  {
+    "revision": "530c17cce8ca8683144d",
+    "url": "/list/static/js/2.525bcecf.chunk.js"
+  },
+  {
+    "revision": "83404bcc06fe7c695f1c",
+    "url": "/list/static/css/main.8f88cd19.chunk.css"
+  },
+  {
+    "revision": "1ab9deb184327f15f917d2902d7cac78",
+    "url": "/list/index.html"
+  }
+];
